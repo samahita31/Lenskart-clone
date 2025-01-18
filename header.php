ee0aeea19505639cdbb67f8e98e02663e9340878
Samahita <samahitakhilar@gmail.com>
2025-01-18 11:50:30 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/lens.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Latest compiled JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+  <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <script src="js/lens.js"></script>
+
+  <title>Lenskart</title>
+</head>
+
+<body>
+  <section class="glass">
+    <div class="kart">
+      <div class="container">
+        <div class="row d-flex justify-content-between">
+          <div class="sub">
+            <ul class="kl">
+              <li><a href="#" style="color: black;text-decoration: none;margin-left: -121px;">Do More, Be More |</a></li>
+              <li><a href="#" style="color: black;text-decoration: none;margin-left: -39px;">Tryin3D |</a></li>
+              <li><a href="#" style="color: black;text-decoration: none;margin-left: -21px;">StoreLocator |</a></li>
+              <li><a href="#" style="color: black;text-decoration: none;margin-left: -21px;">Singapore |</a></li>
+              <li><a href="#" style="color: black;text-decoration: none;margin-left: -21px;">UAE |</a></li>
+              <li><a href="#" style="color: black;text-decoration: none;margin-left: -21px;">John Jacobs |</a></li>
+              <li><a href="#" style="color: black;text-decoration: none;margin-left: -21px;">Aqualens |</a></li>
+              <li><a href="#" style="color: black;text-decoration: none;margin-left: -21px;">Cobrowsing |</a></li>
+              <li><a href="#" style="color: black;text-decoration: none;margin-left: -21px;">Engineering Blog |</a></li>
+              <li><a href="#" style="color: black;text-decoration: none;margin-left: -21px;">Partner With us</a></li>
+            </ul>
+          </div>
+        
+        <ul class="social-links" style="list-style: none; display: inline-block;">
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      </div>
+    </div>
+    </div>
+    </div>
+  </section>
+
+
+  <section>
+    <nav class="navbar navbar-expand-sm navbar-dark " style="margin-top: 3px;">
+      <div class="container">
+        <div class="d-flex">
+          <img src="image/lenslogo.jpg" alt="a" width="7%" height="70px"
+            style="margin-left: -94px; width: 245px; display: block;height:92px; margin-top: -27px;">
+          <img src="image/lens1.jpg" alt="a" width="7%" height="70px"
+            style="width: 129px;height: 39px; margin-left: 69px;">
+
+          <div class="searchbar">
+            <div class="container">
+              <form>
+                <input type="text" autocomplete="false" placeholder="What are you looking for?" value style="width: 447px; margin-left: 55px; border-radius: 5px; height: 36px;">
+              </form>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row d-flex justify-content-between">
+              <ul class="shine" style="list-style: none; display: inline-flex;">
+                <li><a href="#" style="color: black; text-decoration: none; margin-left: 87px; font-size: 14px;">Track Order </a></li>
+                <li><a href="#" style="color: black;text-decoration: none;margin-left: 30px;font-size: 14px;">Sign In & Sign Up </a></li>
+                <li><a href="#" style="margin-left: 27px;"><i class="bi bi-heart"></i></a> Wishlist </li>
+                <li><a href="#" style="margin-left: 28px;"><i class="bi bi-handbag"></i></a> Cart </li>
+              </ul>
+
+
+            </div>
+        </div>
+        </div>
+      </div>
+    </nav>
+    </section>
+
+    <div class="header">
+      <div class="container">
+        <div class="row d-flex justify-content-between">
+          <div class="var">
+          <ul class="type" style="list-style: none; display: inline-flex;">
+            <li><a href="#" style="color: black;text-decoration: none;margin-left: -120px;font-weight: 500;">EYEGLASSES</a></li>
+            <li><a href="#" style="color: black;text-decoration: none;margin-left: -14px;font-weight: 500;">SCREEN GLASSES</a></li>
+            <li><a href="#" style="color: black;text-decoration: none;margin-left:  14px;font-weight: 500;">KIDS GLASSES</a></li>
+            <li><a href="#" style="color: black;text-decoration: none;margin-left:  13px;font-weight: 500;">CONTACT LENSES</a></li>
+            <li><a href="#" style="color: black;text-decoration: none;margin-left:  13px;font-weight: 500;">SUNGLASSES</a></li>
+            <li><a href="#" style="color: black;text-decoration: none;margin-left:  13px;font-weight: 500;">HOME EYE-TEST</a></li>
+            <li><a href="#" style="color: black;text-decoration: none;margin-left:  13px;font-weight: 500;">STORE LOCATOR</a></li>
+          </ul>
+
+        
+        <div class="gal" style="display:inline-flex ;"> 
+          <img src="image/lens3.jpg" alt="" width="73px; height: 42px; style="margin-left: 280px; border-radius: 3px; >
+          <img src="image/lens4.jpg" alt="" width="40px;" >
+          <img src="image/lens5.jpg" alt="" width="40px" >
+        </div>
+      
+    </div>
+    </div>
+</div>
+</div>
