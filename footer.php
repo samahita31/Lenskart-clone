2ee49a0e6401e13756efebf3704fd50b869bd7a0
Samahita <samahitakhilar@gmail.com>
2025-01-18 11:50:03 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer-c">
+  <div class="container">
+  <div class="row">
+<div class="foot" style="background-color: blue; width: 100%;">
+<h1 style="text-align: left;font-size: 32px; font-weight: 480; margin-top: 47px; color: white;">Buy The Best Eyewear From Lenskart</h1>
+<p style= "text-align: left;font-size: 32px; font-weight: 480; margin-top: 47px; color: white;">Lenskart Is The Leading E-Commerce Portal For Eyewear In India. It Has Revolutionised The Eyewear Industry In The Country With Its Omni-Channel Approach. From An Ever-Growing Number Of Offline Stores Across Major Cities In The Country To Innovative Integration Of Technology While Purchasing Online, Lenskart Caters To Every Customer With Several Deals And Offers.</p>
+</div>
+</div>
+</div>
+</div>
